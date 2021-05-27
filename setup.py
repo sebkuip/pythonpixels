fbbe35d14cabb84294d37ef2679a65c1e17130ce
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2021-05-27 21:58:56 +0200

    bump version to 1.2.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=["pythonpixels",],
     install_requires=["pillow",],
     description="An API wrapper for the python discord pixels project",
-    version="1.2.5",
+    version="1.2.6",
     long_description=long_des,
     long_description_content_type="text/markdown",
     license="MIT",
